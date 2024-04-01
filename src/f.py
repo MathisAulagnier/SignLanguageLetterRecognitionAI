1fb8c3ce57836a06c2ca79b01e608bd638e29c6b
mathisaulagnier <mathis.aulagnier@telecomnancy.eu>
2024-04-01 16:46:39 +0200

    Creation d'un model, definition de certain hyperparametres

--- src/f.py
+++ src/f.py
@@ -1,92 +1,30 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
 import numpy as np
-import pandas as pd
-
-class CustomDataset(Dataset):
-    def __init__(self, file_path):
-        self.data = pd.read_csv(file_path)
-        self.X = torch.tensor(self.data.iloc[:, 1:].values, dtype=torch.float32)
-        self.y = torch.tensor(self.data.iloc[:, 0].values, dtype=torch.long)
-        
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        return self.X[idx], self.y[idx]
-    
-class SimpleModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(SimpleModel, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
-        
-    def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
-    
-
-# Hyperparameters
-input_size = 784
-hidden_size = 500
-num_classes = 25
-num_epochs = 5
-batch_size = 100
-learning_rate = 0.001
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-# Load data
+l =np.array([[1.00000000e+01, 1.00000000e-03, 8.83993307e-02],
+    [1.00000000e+01, 1.00000000e-02, 2.00780814e-02],
+    [1.00000000e+01, 1.00000000e-01, 2.00780814e-02],
+    [2.00000000e+01, 1.00000000e-03, 3.06330173e-01],
+    [2.00000000e+01, 1.00000000e-02, 2.00780814e-02],
+    [2.00000000e+01, 1.00000000e-01, 4.32236475e-02],
+    [3.00000000e+01, 1.00000000e-03, 4.40741774e-01],
+    [3.00000000e+01, 1.00000000e-02, 2.28667038e-02],
+    [3.00000000e+01, 1.00000000e-01, 2.28667038e-02],
+    [4.00000000e+01, 1.00000000e-03, 2.06497490e-01],
+    [4.00000000e+01, 1.00000000e-02, 2.02175125e-02],
+    [4.00000000e+01, 1.00000000e-01, 3.43000558e-02],
+    [5.00000000e+01, 1.00000000e-03, 4.49107641e-01],
+    [5.00000000e+01, 1.00000000e-02, 2.00780814e-02],
+    [5.00000000e+01, 1.00000000e-01, 4.00167317e-02]])
 
-train_dataset = CustomDataset('data/sign_mnist_train.csv')
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(l[:, 0], l[:, 1], l[:, 2], c='r', marker='o')
+ax.set_xlabel('Number of Epochs')
+ax.set_ylabel('Learning Rate')
+ax.set_zlabel('Training Accuracy')
+plt.show()
 
-test_dataset = CustomDataset('data/sign_mnist_test.csv')
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
-# Model
-
-model = SimpleModel(input_size, hidden_size, num_classes)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-# Train the model
-def train_model(model, train_loader, num_epochs):
-    total_step = len(train_loader)
-    for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(train_loader):
-            # Reshape images to (batch_size, input_size)
-            images = images.reshape(-1, input_size)
-            
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            if (i+1) % 100 == 0:
-                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-            
-# Test the model
-def test_model(model, test_loader):
-    model.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            images = images.reshape(-1, input_size)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-        print('Accuracy of the network on the test images: {} %'.format(100 * correct / total))
 
